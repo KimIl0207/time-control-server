bf1ff0fb62af8f69afb02603b87b9d71c9840b0b
unknown <so0214n2@gmail.com>
2025-04-14 12:19:41 +0900

    Delete unnecessary files from git

--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 시스템 시작
-start "" /min pythonw.exe server.py
-timeout /t 2 > nul
-start "" /min python shutDownManager.py
--- start.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "run_timer.bat", 0, False
